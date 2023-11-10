dba29051b80ef58c1a4b2b83a82c18c640f4c16b
erlonbie <erlonminievix@gmail.com>
2023-11-10 12:03:55 +0000

    tcp stuff

--- tcp_listener/src/main.rs
+++ tcp_listener/src/main.rs
@@ -1,29 +1,52 @@
+use std::fmt;
 use std::fmt::{Display, Pointer};
+use std::io::Write;
 use std::io::{BufRead, BufReader};
 use std::net::{TcpListener, TcpStream};
 use std::result;
+use std::{
+    sync::mpsc::{channel, Receiver, Sender},
+    thread,
+};
 
 type Result<T> = result::Result<T, ()>;
 
-fn handle_client(stream: TcpStream) {
-    // let mut reader = BufReader::new(stream.try_clone().unwrap());
-    // let mut writer = BufWriter::new(stream);
+// fn handle_client(stream: TcpStream) {
+//     // let mut reader = BufReader::new(stream.try_clone().unwrap());
+//     // let mut writer = BufWriter::new(stream);
+//
+//     let mut reader = BufReader::new(stream);
+//
+//     let mut line = String::new();
+//     reader.read_line(&mut line).unwrap();
+//     println!("Received: {}", line.trim());
+//
+//     // writer.write_all(line.as_bytes()).unwrap();
+//     // writer.flush().unwrap();
+// }
 
-    let mut reader = BufReader::new(stream);
+enum Message {
+    ClientConnected,
+    ClientDisconected,
+    NewMessage
+}
 
-    let mut line = String::new();
-    reader.read_line(&mut line).unwrap();
-    println!("Received: {}", line.trim());
+fn server() {
 
-    // writer.write_all(line.as_bytes()).unwrap();
-    // writer.flush().unwrap();
+}
+
+fn client(mut stream: TcpStream) {
+    let _ = writeln!(stream, "Ola, cliente!").map_err(|err| {
+        eprintln!("Could not write user message: {err}");
+    });
+    todo!()
 }
 
 const SAFE_MODE: bool = true;
 
 struct Sensitve<T>(T);
 
-impl<T> Display  for Sensitve<T> {
+impl<T> Display for Sensitve<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let Self(inner) = self;
         if SAFE_MODE {
@@ -39,13 +62,16 @@ fn main() -> Result<()> {
     let listener = TcpListener::bind(adress).map_err(|err| {
         eprintln!("ERROR: could not bind {adress}: {err}");
     })?;
-
+    println!("listening on {}", Sensitve(adress));
     for stream in listener.incoming() {
-        let stream = stream.map_err(|err| {
-            eprintln!("ERROR: could not connect to client: {err}");
-        })?;
-        println!("listening on {}, {}",Sensitve(adress), Sensitve(stream.peer_addr().unwrap()));
-        handle_client(stream);
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| client(stream));
+            }
+            Err(err) => {
+                eprint!("Could not spawn a thread: {}", err);
+            }
+        }
     }
     Ok(())
 }
