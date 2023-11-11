c1ee4a2e59ace0ba16c4165681403405bbc657ae
erlonbie <erlonminievix@gmail.com>
2023-11-11 10:03:05 -0400

    tcp client done

--- tcp_listener/src/main.rs
+++ tcp_listener/src/main.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
 use std::fmt::{Display, Pointer};
-use std::io::{Read};
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::result;
 use std::sync::Arc;
@@ -25,28 +26,69 @@ type Result<T> = result::Result<T, ()>;
 // }
 
 enum Message {
-    ClientConnected(Arc<TcpStream>),
-    ClientDisconected(Arc<TcpStream>),
-    NewMessage(Vec<u8>),
+    ClientConnected {
+        author: Arc<TcpStream>,
+    },
+    ClientDisconected {
+        author: Arc<TcpStream>,
+    },
+    NewMessage {
+        author: Arc<TcpStream>,
+        bytes: Vec<u8>,
+    },
+}
+
+struct Client {
+    conn: Arc<TcpStream>,
 }
 
 fn server(messages: Receiver<Message>) {
-    todo!()
+    let mut clients = HashMap::new();
+    loop {
+        let msg = messages.recv().expect("could not receive message");
+        match msg {
+            Message::ClientConnected { author } => {
+                let addr = author.as_ref().peer_addr().unwrap();
+                clients.insert(addr, Client { conn: author });
+            }
+            Message::ClientDisconected { author } => {
+                let addr = author.as_ref().peer_addr().unwrap();
+                clients.remove(&addr);
+            }
+            Message::NewMessage { author, bytes } => {
+                let author_addr = author.as_ref().peer_addr().unwrap();
+                for (addr, client) in clients.iter() {
+                    if *addr != author_addr {
+                        let _ = client.conn.as_ref().write_all(&bytes);
+                    }
+                }
+            }
+        }
+    }
 }
 
-fn client(mut stream: Arc<TcpStream>, messages: Sender<Message>) -> Result<()> {
-    messages.send(Message::ClientConnected(stream.clone())).map_err(|err| {
-        eprintln!("ERROR: could connect to the server thread: {err}");
-    })?;
+fn client(stream: Arc<TcpStream>, messages: Sender<Message>) -> Result<()> {
+    messages
+        .send(Message::ClientConnected {
+            author: stream.clone(),
+        })
+        .map_err(|err| {
+            eprintln!("ERROR: could connect to the server thread: {err}");
+        })?;
     let mut buffer = Vec::new();
     buffer.resize(64, 0);
     loop {
         let n = stream.as_ref().read(&mut buffer).map_err(|err| {
             eprintln!("ERROR: could not read messagem from the client: {err}");
-            let _ = messages.send(Message::ClientDisconected(stream.clone()));
+            let _ = messages.send(Message::ClientDisconected {
+                author: stream.clone(),
+            });
         })?;
         messages
-            .send(Message::NewMessage(buffer[0..n].to_vec()))
+            .send(Message::NewMessage {
+                author: stream.clone(),
+                bytes: buffer[0..n].to_vec(),
+            })
             .map_err(|err| {
                 eprintln!("ERROR: could connect to the server thread: {err}");
             })?;
@@ -69,7 +111,7 @@ impl<T> Display for Sensitve<T> {
 }
 
 fn main() -> Result<()> {
-    let adress = "127.0.0.1:6969";
+    let adress = "0.0.0.0:6969";
     let listener = TcpListener::bind(adress).map_err(|err| {
         eprintln!("ERROR: could not bind {adress}: {err}");
     })?;
